ee8b817f6c3eefd835cbf03fdd7aff8932694efa
DeelakaM4 <deelaka.mahale@gmail.com>
2021-07-07 12:43:10 +0530

    Update PlayerSelection.java

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -10,5 +10,41 @@ public class PlayerSelection
 		ArrayList <Player> toyList = new ArrayList<Player>();
 		String menuOption, playerName, playerAge, playerType;
 		double bowlingAverage, battingAverage;
+		
+		System.out.println("");
+		System.out.println("Player Selection");
+		System.out.println("");
+		
+		while (true)
+		{
+			System.out.println("");
+			
+			if (menuOption.equals("A"))// Add a player
+			{
+				
+			}
+			else if (menuOption.equals("B"))// Display to best batsmen
+			{
+				
+			}
+			else if (menuOption.equals("C"))// Display to best bowlers
+			{
+				
+			}
+			else if (menuOption.equals("D"))// Display the best keeper
+			{
+				
+			}
+			else if (menuOption.equals("E"))// Exit
+			{
+				
+			}
+			else
+			{
+				System.out.println("Invalid input!");
+			}
+			
+			System.out.println("");
+		}
 	}
 }
