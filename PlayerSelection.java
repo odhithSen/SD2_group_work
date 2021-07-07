f7c9b72bec87cd7d59f9f2cac388d8b44d609705
DeelakaM4 <deelaka.mahale@gmail.com>
2021-07-07 12:22:32 +0530

    Update PlayerSelection.java
    Array list part and initialization

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -1,9 +1,13 @@
 // Task 2
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class PlayerSelection
 {
 	public static void main(String[] args)
 	{
-		
+		ArrayList <Player> toyList = new ArrayList<Player>();
+		String menuOption, playerName;
 	}
 }
